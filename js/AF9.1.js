6c3758e289fe1a2b6c1efb005f5f45cb24fff648
Luis-Alejandro-Rodriguez-Fernandez <rodriguezfernandezluis01@gmail.com>
2022-02-17 10:55:43 +0100

    Commit inicial

---
+++ js/AF9.1.js
@@ -0,0 +1,27 @@
+// write here the optimal jQuery code as required in the statemens
+// $("section").mouseenter(function () {
+//   $(this).children().css({ opacity: "1", transition: "2s" });
+// });
+// $("section").mouseleave(function () {
+//   $(this).find("a").css({ opacity: "0", transition: "2s" });
+// });
+
+$("a").mouseenter(function () {
+  let pais = $(this).siblings("h2").text();
+  let foto1=`images/${pais.toLowerCase()}.jpg`;
+  let foto2=`images/${pais.toLowerCase()}.logo.png`;
+  $("header img").first().attr("src", foto1);
+  $("header img").last().attr("src", foto2);
+
+  $(this).next().show(600);
+  $(this).siblings("div").toggle();
+
+});
+
+$("a").mouseleave(function () {
+  let foto1 = "images/europe1.jpg";
+  let foto2 = "images/europe2.jpg";
+  $(this).siblings("div").toggle();
+  $("header img").first().attr("src", foto1);
+  $("header img").last().attr("src", foto2);
+});
---
+++ js/date.en.js
@@ -0,0 +1,41 @@
+﻿var months = new Array(
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+);
+
+var WeekDays = new Array(
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+);
+
+var f = new Date(); //javascript
+
+var strDate =
+  WeekDays[f.getDay()] +
+  " - " +
+  months[f.getMonth()] +
+  " " +
+  f.getDate() +
+  ", " +
+  f.getFullYear();
+
+  /* con el método jQuery adecuado, asignar  el string strDate que contiene la fecha obtenida con la función Date() de javascript a un elemento HTML que has de incluir en el footer para tal efecto */
+$(document).ready(function () {
+  $("h3+h3").html(strDate);
+});
+
